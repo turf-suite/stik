8d4dd3ecadad7d05e5543d36310d98874537e611
Ryan Monahan <ryanaldo34@gmail.com>
2024-07-20 16:17:26 -0500

    add todos table

---
+++ supabase/migrations/20240719012653_todos.sql
@@ -0,0 +1,24 @@
+create table
+    if not exists public.profiles (
+        user_id uuid not null,
+        birthday date not null,
+        first_name text not null,
+        last_name text not null,
+        middle_name text null,
+        phone text null,
+        avatar text null,
+        constraint profiles_pkey primary key (user_id),
+        constraint profiles_phone_key unique (phone),
+        constraint profiles_id_fkey foreign key (user_id) references auth.users (id) on delete cascade
+    ) tablespace pg_default;
+
+create table
+    if not exists public.todos (
+        todo_id bigserial primary key,
+        title text not null,
+        description text,
+        owner uuid not null,
+        constraint profiles_fkey foreign key (owner) references public.profiles (user_id) on delete cascade,
+        completed_at timestamp,
+        created_at timestamp
+    ) tablespace pg_default;
